ddf8daaa58ac070bf59654180c7bfd135d83c669
fazer06 <shaun.ttre@gmail.com>
2016-02-12 13:17:52 +0000

    Deleted unwanted css files, added custom validation message to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,12 @@
 class Article < ActiveRecord::Base
+	
 	has_many :comments
-	validates :title, presence: true, length: { minimum: 5 }
+
+	validates :title, presence: true, length: { minimum: 5,
+    too_short: "%{count} characters is the maximum allowed" }
+
 	validates :body,  presence: true, length: { minimum: 10 }
+
+	validates_associated :comments
 
 end
